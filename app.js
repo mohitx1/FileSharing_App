65ed3d58f366ffeecfc78a9a06a92e3ce6f314e3
mohitx1 <mohitrockx3@gmail.com>
2023-09-16 03:57:08 +0530

    added functionality to download file visiting /uuid

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const port=process.env.PORT|| 3000;
 const connectDB=require('./config/db');
 connectDB();
 
+app.use(express.static('public'));
+
 //TemplateEngine
 app.set('views', path.join(__dirname,'/views'))
 app.set('view engine', 'ejs')
@@ -15,6 +17,7 @@ app.set('view engine', 'ejs')
 //Routes
 app.use('/api/files',require('./routes/rfiles'))
 app.use('/files',require('./routes/show'))
+app.use('/files/download',require('./routes/download'));
 
 app.listen(port,()=>{
     console.log(`Server running at port: ${port}`)
--- models/mfiles.js
+++ models/mfiles.js
@@ -3,7 +3,7 @@ const Schema= mongoose.Schema;
 
 
 const fileSchema=new Schema({
-    fileName:{type: String, required:true},
+    filename:{type: String, required:true},
     path:{type: String, required: true},
     size:{type: Number, required:true},
     uuid:{type:String, required:true},
