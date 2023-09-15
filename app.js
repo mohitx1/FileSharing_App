0cc5cec7cf494a2d3366974f271cc8f78112d43a
mohitx1 <mohitrockx3@gmail.com>
2023-09-16 03:07:23 +0530

    push origin master

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ const port=process.env.PORT|| 3000;
 const connectDB=require('./config/db');
 connectDB();
 
+//TemplateEngine
+app.set('views', path.join(__dirname,'/views'))
+app.set('view engine', 'ejs')
+
 //Routes
 app.use('/api/files',require('./routes/rfiles'))
+app.use('/files',require('./routes/show'))
 
 app.listen(port,()=>{
     console.log(`Server running at port: ${port}`)
